3170273320b6198c74a0e7bc9b821232a233f214
Sean Barrett <sean.barrett14@imperial.ac.uk>
2017-11-02 00:29:51 +0000

    Trace some rays

---
+++ tracer.py
@@ -0,0 +1,161 @@
+import math
+import numpy as np
+import scipy.integrate as spi
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+a = 0.0  # black hole angular momentum
+n_rays = 24
+nt = 10000  # time steps (time points - 1)
+
+
+def rho(y):
+    r, theta, phi, p_r, p_theta, p_phi = y
+    cost = math.cos(theta)
+    return math.sqrt(r * r + a * a * cost * cost)
+
+
+def rho_sqr(y):
+    r, theta, phi, p_r, p_theta, p_phi = y
+    cost = math.cos(theta)
+    return r * r + a * a * cost * cost
+
+
+def Sigma(y):
+    r, theta, phi, p_r, p_theta, p_phi = y
+    sint = math.sin(theta)
+    rr_plus_aa = r * r + a * a
+    return math.sqrt(rr_plus_aa * rr_plus_aa - a * a * sint * sint)
+
+
+def Delta(y):
+    r, theta, phi, p_r, p_theta, p_phi = y
+    return r * r - 2 * r + a * a
+
+
+def omega(y):
+    r, theta, phi, p_r, p_theta, p_phi = y
+    sig = Sigma(y)
+    return 2 * a * r / (sig * sig)
+
+
+def omega_bar(y):
+    r, theta, phi, p_r, p_theta, p_phi = y
+    return Sigma(y) * math.sin(theta) / rho(y)
+
+
+def alpha(y):
+    return rho(y) * math.sqrt(Delta(y)) / Sigma(y)
+
+
+def E_f(y, n_phi):
+    r, theta, phi, p_r, p_theta, p_phi = y
+    return 1 / (alpha(y) + omega(y) * omega_bar(y) * n_phi)
+
+
+def q(y):
+    r, theta, phi, p_r, p_theta, p_phi = y
+    cost = math.cos(theta)
+    sint = math.sin(theta)
+    return p_theta * p_theta + cost * cost * (p_phi * p_phi / (sint * sint) - a * a)
+
+
+def P(y):
+    r, theta, phi, p_r, p_theta, p_phi = y
+    return r * r + a * a - a * p_phi
+
+
+def R(y):
+    r, theta, phi, p_r, p_theta, p_phi = y
+    p = P(y)
+    p_minus_a = (p_phi - a)
+    return p * p - Delta(y) * (p_minus_a * p_minus_a + q(y))
+
+
+def Theta(y):
+    r, theta, phi, p_r, p_theta, p_phi = y
+    cost = math.cos(theta)
+    sint = math.sin(theta)
+    return q(y) - cost * cost * (p_phi * p_phi / (sint * sint) - a * a)
+
+
+def deriv(y, zeta):
+    r, theta, phi, p_r, p_theta, p_phi = y
+    _delta = Delta(y)
+    _rho_sqr = rho_sqr(y)
+    _rho_qua = _rho_sqr * _rho_sqr
+    _P = P(y)
+    _R = R(y)
+    _Theta = Theta(y)
+    _b = p_phi
+    _q = q(y)
+
+    cot = -math.tan(theta - math.pi / 2.0)
+    aasincos = a * a * math.sin(theta) * math.cos(theta)
+
+    dr = p_r * _delta / _rho_sqr
+    dtheta = p_theta * 1.0 / _rho_sqr
+    dphi = (a * _P / _delta + _b - a + _b * cot * cot) / _rho_sqr
+    dp_r = p_r * p_r * (_delta * r / _rho_qua - (r - 1) / _rho_sqr) + \
+        p_theta * p_theta * r / _rho_qua + \
+        ((2 * r * _P - (r - 1) * ((_b - a) * (_b - a) + _q)) * _delta * _rho_sqr -
+         _R * ((r - 1) * _rho_sqr + _delta * r)) / (_delta * _delta * _rho_qua) - \
+        r * _Theta / _rho_qua
+    dp_theta = -aasincos * _delta * p_r * p_r / _rho_qua - \
+        aasincos * p_theta * p_theta / _rho_qua + \
+        (_R * aasincos - _delta * _rho_sqr * (aasincos - _b * _b * (cot + cot * cot * cot))) \
+         / (_delta * _rho_qua) + \
+        _Theta * aasincos / _rho_qua
+    dp_phi = 0
+
+    return np.array([dr, dtheta, dphi, p_r, p_theta, p_phi])
+
+# [ray_0, ... , ray_n-1]
+# [r, theta, phi, p_r, p_theta, p_phi]
+rays_0 = np.zeros((n_rays, 6))
+
+# camera position (r, theta, phi)
+cam_pos = np.array([10, 0.5 * np.pi, 0])
+
+#  t=0 ray directions in camera's spherical coordinates (aligned to FIDO)
+theta_0 = np.linspace(0, np.pi, n_rays)
+phi_0 = np.linspace(0, 2 * np.pi, n_rays)
+# unit vectors in tangent space to FIDO, up is along e_theta
+n_0 = np.zeros((n_rays, 3))  # (r,theta,phi)
+n_0[:, 0] = -np.sin(theta_0) * np.cos(phi_0)
+n_0[:, 2] = -np.sin(theta_0) * np.sin(phi_0)
+n_0[:, 1] = -np.cos(theta_0)
+
+# initialise ray momenta and positions
+# Note: E_f doesnt depend on momentum
+y_0 = np.concatenate((cam_pos, np.zeros(3))) 
+
+for i in range(n_rays):
+    rays_0[i, 3] = n_0[i, 0] * E_f(y_0, n_0[i, 2]) * rho(y_0) / math.sqrt(Delta(y_0))
+    rays_0[i, 4] = n_0[i, 1] * E_f(y_0, n_0[i, 2]) * rho(y_0)
+    rays_0[i, 5] = n_0[i, 2] * E_f(y_0, n_0[i, 2]) * omega_bar(y_0)  # = b (conserved)
+
+rays_0[:, 0:3] = cam_pos.copy()
+
+zeta = np.linspace(0, -10, nt + 1) # Think carefully about this (TODO)
+
+rays = np.zeros((n_rays, nt + 1, 6))
+
+# integrate momenta and positions
+for i in range(n_rays):
+    rays[i] = spi.odeint(deriv, rays_0[i], zeta)
+
+# plot
+rays_x = np.sqrt(rays[:,:, 0]**2 + a * a) * \
+    np.sin(rays[:,:, 1]) * np.cos(rays[:,:, 2])
+rays_y = np.sqrt(rays[:,:, 0]**2 + a * a) * \
+    np.sin(rays[:,:, 1]) * np.sin(rays[:,:, 2])
+rays_z = rays[:,:, 0] * np.cos(rays[:,:, 1])
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+for i in range(n_rays):
+    ax.plot(rays_x[i,:], rays_y[i,:], zs=rays_z[i,:])
+
+plt.show()
