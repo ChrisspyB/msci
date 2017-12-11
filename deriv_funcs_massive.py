Added correct energy calculation.

deriv_funcs_massive.py
@@ -1,94 +1,130 @@
+# NOTE: All equations here assume mu = 1
+
 import math
 import numpy as np
 
 def rho(y, a):
-    t, r, theta, phi = y
+    t, r, theta, phi, p_r, p_theta = y
     cost = math.cos(theta)
     return math.sqrt(r * r + a * a * cost * cost)
 
-
 def rho_sqr(y, a):
-    t, r, theta, phi = y
+    t, r, theta, phi, p_r, p_theta = y
     cost = math.cos(theta)
     return r * r + a * a * cost * cost
 
-
-def Sigma(y, a):
-    t, r, theta, phi = y
-    sint = math.sin(theta)
-    rr_plus_aa = r * r + a * a
-    return math.sqrt(rr_plus_aa * rr_plus_aa - a * a * sint * sint)
-
-
 def Delta(y, a):
-    t, r, theta, phi = y
+    t, r, theta, phi, p_r, p_theta = y
     return r * r - 2 * r + a * a
 
-
-def omega(y, a):
-    t, r, theta, phi = y
-    sig = Sigma(y, a)
-    return 2 * a * r / (sig * sig)
-
-
-def pomega(y, a):
-    t, r, theta, phi = y
-    return Sigma(y, a) * math.sin(theta) / rho(y, a)
-
-
-def alpha(y, a):
-    return rho(y, a) * math.sqrt(Delta(y, a)) / Sigma(y, a)
-
-
-def q(y, p_theta, a, musq, E, b):
-    t, r, theta, phi = y
+def q(theta, p_theta, a, E, b):
     cost = math.cos(theta)
     sint = math.sin(theta)
-    return p_theta * p_theta + cost * cost * (b * b / (sint * sint) + a * a * (musq-E*E))
-
+    return p_theta * p_theta + cost * cost * (b * b / (sint * sint) + a * a * (1-E*E))
 
 def P(y, a, E, b):
-    # == P
-    t, r, theta, phi = y
+    t, r, theta, phi, p_r, p_theta = y
     return E*(r * r + a * a) - a * b
 
-
-def R(y, a, musq, E, b, q):
-    t, r, theta, phi = y
+def R(y, a, E, b, q):
+    t, r, theta, phi, p_r, p_theta = y = y
     p = P(y, a, E, b)
     b_minus_aE = (b - a*E)
-    return p * p - Delta(y, a) * (musq*r*r + b_minus_aE * b_minus_aE + q)
+    return p * p - Delta(y, a) * (r*r + b_minus_aE * b_minus_aE + q)
 
-
-def Theta(y, a, musq, E, b, q):
-    t, r, theta, phi = y
+def Theta(y, a, E, b, q):
+    t, r, theta, phi, p_r, p_theta = y
     cost = math.cos(theta)
     sint = math.sin(theta)
-    return q - cost * cost * (b * b / (sint * sint) + a * a * (musq-E*E))
+    return q - cost * cost * (b * b / (sint * sint) + a * a * (1-E*E))
+#    return p_theta*p_theta
 
-def deriv_massive(y, zeta, a, mu, E, b, q):
-    musq = mu*mu
-    
-    # note different y
-    t, r, theta, phi = y
+def deriv(y, zeta, a, E, b, q):   
+    t, r, theta, phi, p_r, p_theta = y
 
     _delta = Delta(y, a)
     _rho_sqr = rho_sqr(y, a)
     _P = P(y, a, E, b)
-    _R = R(y, a, musq, E, b, q)
-    _Theta = Theta(y, a, musq, E, b, q)
-    _Lz = b
-
-    sinsq = math.sin(theta) * math.sin(theta)
-
-    dtheta = math.sqrt(_Theta)
-    dr = math.sqrt(_R)
-    dphi = -(a * E - _Lz / sinsq) + _P * a / _delta
-    dt = -a * (a * E * sinsq - _Lz) + _P * (r * r + a * a)/_delta
+    _R = R(y, a, E, b, q)
+    _Theta = Theta(y, a,  E, b, q)
     
-    # reached event horizon
+    cost = math.cos(theta)
+    sint = math.sin(theta)
+    cott = -math.tan(theta - math.pi / 2.0)
+    
+    sinsq = sint * sint
+    cossq = cost * cost
+    cotsq = cott * cott
+    
+    dt = (_P*(r*r+a*a)+a*_delta*(b-a*E*+a*E*cossq) )/(_delta*_rho_sqr)
+    dr = _delta * p_r / _rho_sqr
+    dtheta = p_theta / _rho_sqr
+    dphi = (a*_P+_delta*(b-a*E+b*cotsq))/(_rho_sqr*_delta)
+    
+    dRdr = 4*E*r*_P-(2*r-2)*(r*r+(b-a*E)**2+q)-2*_delta*r
+    dp_r = (p_theta*p_theta-p_r*p_r*(_rho_sqr*(r-1)/r -_delta))*r/(_rho_sqr*_rho_sqr) + (dRdr+2*(r-1)*_Theta - (_R+_delta*_Theta)*(2*(r-1)/_delta + 2*r/_rho_sqr))/(2*_delta*_rho_sqr)
+    
+    dqdtheta = -2*sint*cost*(a*a*(1-E*E)+b*b/sinsq)-2*b*b*cotsq*cott
+    dp_theta = -a*a*cost*sint*(p_theta*p_theta+_delta*p_r*p_r)/_rho_sqr*_rho_sqr + (a*a*cost*sint*(_R+_delta*_Theta)/_rho_sqr-_delta*dqdtheta/2)/(_delta*_rho_sqr)
+    
+    # close to event horizon
     if (r - 2) < 1e-6:
-        return np.zeros(4)
+        print("TERMINATED")
+        return np.zeros(6)
         
+    return np.array([dt, dr, dtheta, dphi, dp_r, dp_theta])
 
-    return np.array([dt,dr,dtheta,dphi])/_rho_sqr
+# covariant
+def metric(y, a):
+    _, r, theta, _, _, _ = y
+    
+    _rho_sqr = rho_sqr(y, a)
+    sinsq = math.sin(theta)*math.sin(theta)
+    
+    gtt = -(1 - 2*r/_rho_sqr)
+    gtphi = -2*a*r*sinsq/_rho_sqr
+    grr = _rho_sqr / Delta(y, a)
+    gthth = _rho_sqr
+    gphiphi = sinsq * (r*r + a*a + 2*r*a*a*sinsq/_rho_sqr)
+    
+    return np.array([
+                [gtt, 0, 0, gtphi],
+                [0, grr, 0, 0],
+                [0, 0, gthth, 0],
+                [gtphi, 0, 0, gphiphi]
+            ])
+    
+# contravariant
+def inv_metric(y, a):
+    _, r, theta, _, _, _ = y
+    
+    g = metric(y, a)
+    
+    gtt = g[0,0]
+    gtphi = g[0, 3]
+    grr = g[1,1]
+    gthth = g[2,2]
+    gphiphi = g[3,3]
+    
+    div = 1/(gphiphi*gtt - gtphi*gtphi)
+    
+    return np.array([
+                [gphiphi*div, 0, 0, -gtphi*div],
+                [0, 1/grr, 0, 0],
+                [0, 0, 1/gthth, 0],
+                [-gtphi*div, 0, 0, gtt*div]
+            ])
+    
+def energy(y, a, b):
+    _, _, _, _, p_r, p_theta = y
+    
+    invg = inv_metric(y,a)
+    
+    _a = invg[0,0]
+    _b = -2*b*invg[0,3]
+    _c = 1 + p_r*p_r*invg[1,1] + p_theta*p_theta*invg[2,2] + b*b*invg[3,3]
+    
+    right = math.sqrt(_b*_b - 4*_a*_c)/(2*_a)
+    left = -_b/(2*_a)
+    
+    return max(left - right, left + right)

horiz_kerr_deflection.py
@@ -4,7 +4,7 @@ import scipy.integrate as spi
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-from deriv_funcs_light import deriv_light, E_f, rho, Delta, pomega
+from deriv_funcs_light import deriv, E_f, rho, Delta, pomega
 
 SAVE = False
 PLOT = True
@@ -63,7 +63,7 @@ rays = np.zeros((n_rays, nt + 1, 6))
 deflec = np.zeros(n_rays)
 # integrate momenta and positions
 for i in range(n_rays):
-    rays[i] = spi.odeint(deriv_light, rays_0[i], zeta, (a,))
+    rays[i] = spi.odeint(deriv, rays_0[i], zeta, (a,))
 
 rays_x = np.sqrt(rays[:,:, 0]**2 + a * a) * \
     np.sin(rays[:,:, 1]) * np.cos(rays[:,:, 2])

massive.py
@@ -3,55 +3,62 @@ import scipy.integrate as spi
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-from newderiv_funcs_massive import deriv_massive, q
+from deriv_funcs_massive import deriv, q, energy
+
+# ----
+# Roughly reproduces FIG. 2 of Levin 2008
+# ----
 
 SAVE = 0
 PLOT = 1
 
-n_orbits = 1
+nt = 10000
 
-nt = 100
+T = 10000
 
-T = 25 # units of affine parameter?
+a = 0.0  # black hole angular momentum
 
-a = 0.99999999  # black hole angular momentum
+# t, r, theta, phi, p_r, p_theta = y
 
-# r, theta, phi, p_r, p_theta, t = y
+# units of length are half the schwarzschild radius (also note c = 1)
+t0 = 0
+r0 = 27.75
+theta0 = np.pi/2
+phi0 = 0
 
-orbits_0 = np.zeros((n_orbits, 6))
-r0, theta0, phi0, t0 = 4, 0.1 * np.pi, 0.5 * np.pi, 0
-p_r0, p_theta0 = 0.2,0.2
-y_0 = np.array([r0,theta0,phi0,p_r0,p_theta0,t0])
-orbits_0[:] = y_0
+# these are componenets of the covariant 4-momentum (= 4-velocity, as mu = 1)
+p_r0 = 0
+p_theta0 = 0.0
+p_phi = 3.980393
+
+y_0 = np.array([t0, r0, theta0, phi0, p_r0, p_theta0])
+
+# these are functions of _0:
+# angular momentum (= r * p^phi)
+b = p_phi
+# energy at infinity
+#E =  0.973101
+E = energy(y_0, a, b)
+# Carter's constant
+_q = q(theta0, p_theta0, a, E, b)
 
 zeta = np.linspace(0, T, nt + 1)
-orbits = np.zeros((n_orbits, nt + 1, 6))
+orbit = np.zeros((nt + 1, 6))
 
-# integrate momenta and positions
-for i in range(n_orbits):
-    
-    # here units of length are half the schwarzschild radius (also note c = 1)
-    b = 0.2 # p_phi
-    E = 1 # energy at infinity
-    
-    _q = q(y_0, p_theta0, a, E, b)
-    
-    # TODO: CHECK b,p_theta,p_r,E are physically allowed
-    orbits[i] = spi.odeint(deriv_massive, orbits_0[i], zeta, (a,E,b,_q))
+orbit = spi.odeint(deriv, y_0, zeta, (a,E,b,_q), atol = 1e-10)
 
-orbits_x = np.sqrt(orbits[:, :, 1]**2 + a * a) * \
-    np.sin(orbits[:, :, 2]) * np.cos(orbits[:, :, 3])
-orbits_y = np.sqrt(orbits[:, :, 1]**2 + a * a) * \
-    np.sin(orbits[:, :, 2]) * np.sin(orbits[:, :, 3])
-orbits_z = orbits[:, :, 1] * np.cos(orbits[:, :, 2])
+orbit_x = np.sqrt(orbit[:, 1]**2 + a * a) * \
+    np.sin(orbit[:, 2]) * np.cos(orbit[:, 3])
+orbit_y = np.sqrt(orbit[:, 1]**2 + a * a) * \
+    np.sin(orbit[:, 2]) * np.sin(orbit[:, 3])
+orbit_z = orbit[:, 1] * np.cos(orbit[:, 2])
 
 if SAVE:
-    np.save("renderdata", np.dstack((orbits_x, orbits_y, orbits_z)))
+    np.save("renderdata", np.dstack((orbit_x, orbit_y, orbit_z)))
 if PLOT:
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.set_zlim(-1,1)
-    for i in range(n_orbits):
-        ax.plot(orbits_x[i, :], orbits_y[i, :], zs=orbits_z[i, :])
+    ax.set_zlim(-5,5)
+    ax.plot(orbit_x, orbit_y, zs=orbit_z)
 
     plt.show()

schwarzschild_deflection.py
@@ -4,7 +4,7 @@ import scipy.integrate as spi
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-from deriv_funcs_light import deriv_light, E_f, rho, Delta, pomega
+from deriv_funcs_light import deriv, E_f, rho, Delta, pomega
 
 SAVE = False
 PLOT = True
@@ -33,14 +33,11 @@ cam_pos[:, 2] = np.arctan2(cam_y, cam_x)
 # initial (negative) ray directions in camera's cartesian coords
 # unit vectors in tangent space to FIDO
 n_0 = np.zeros((n_rays, 3))  # (e_r, e_theta, e_phi)
-# e_x
 n_0[:, 0] = np.cos(cam_pos[:, 2])
 n_0[:, 1] = 0
 n_0[:, 2] = -np.sin(cam_pos[:, 2])
 
-# note the minus signs above; rays are integrated backwards in time
-# and we would like to specify theta_0, phi_0 in the direction of
-# integration of the rays
+# note the signs above; rays are integrated backwards in time
 
 # initialise ray momenta and positions
 # Note: E_f doesnt depend on momentum
@@ -61,7 +58,7 @@ rays = np.zeros((n_rays, nt + 1, 6))
 deflec = np.zeros(n_rays)
 # integrate momenta and positions
 for i in range(n_rays):
-    rays[i] = spi.odeint(deriv_light, rays_0[i], zeta, (a,))
+    rays[i] = spi.odeint(deriv, rays_0[i], zeta, (a,))
 
 rays_x = np.sqrt(rays[:,:, 0]**2 + a * a) * \
     np.sin(rays[:,:, 1]) * np.cos(rays[:,:, 2])