Fixed derivative function.

deriv_funcs_massive.py
@@ -65,13 +65,13 @@ def deriv(y, zeta, a, E, b, q):
     dp_r = (p_theta*p_theta-p_r*p_r*(_rho_sqr*(r-1)/r -_delta))*r/(_rho_sqr*_rho_sqr) + (dRdr+2*(r-1)*_Theta - (_R+_delta*_Theta)*(2*(r-1)/_delta + 2*r/_rho_sqr))/(2*_delta*_rho_sqr)
     
     dqdtheta = -2*sint*cost*(a*a*(1-E*E)+b*b/sinsq)-2*b*b*cotsq*cott
-    dp_theta = -a*a*cost*sint*(p_theta*p_theta+_delta*p_r*p_r)/_rho_sqr*_rho_sqr + (a*a*cost*sint*(_R+_delta*_Theta)/_rho_sqr-_delta*dqdtheta/2)/(_delta*_rho_sqr)
+    dp_theta = -a*a*cost*sint*(p_theta*p_theta+_delta*p_r*p_r)/(_rho_sqr*_rho_sqr) + (a*a*cost*sint*(_R+_delta*_Theta)/_rho_sqr-_delta*dqdtheta/2)/(_delta*_rho_sqr)
     
     # close (or past) to event horizon
     # TODO: do this better (dependent on a)
-    if (r - 1) < 1e-6:
-        print("TERMINATED")
-        return np.zeros(6)
+#    if (r - 1) < 1e-6:
+#        print("TERMINATED")
+#        return np.zeros(6)
         
     return np.array([dt, dr, dtheta, dphi, dp_r, dp_theta])
 

s2.py
@@ -107,8 +107,7 @@ mat = np.array([
 
 _p[1:4] = np.linalg.solve(mat, v_bh)
 
-_time = time_contra(r0,theta0,_p[1],_p[2],_p[3],a)
-_p[0] = _time
+_p[0] = time_contra(r0,theta0,_p[1],_p[2],_p[3],a)
 
 # multiply by metric for covariant 4-momentum
 metric0 = metric(np.array([0, r0, theta0, 0, 0, 0]), a) # only depends on r, theta
@@ -130,6 +129,10 @@ _q = q(theta0, p_theta0, a, E, b)
 
 zeta = np.linspace(0, T, nt + 1)
 orbit = np.zeros((nt + 1, 6))
+orbit_x = np.zeros((nt + 1, 6))
+orbit_y = np.zeros((nt + 1, 6))
+orbit_z = np.zeros((nt + 1, 6))
+
 
 orbit = spi.odeint(deriv, y_0, zeta, (a,E,b,_q), atol = 1e-10)
 