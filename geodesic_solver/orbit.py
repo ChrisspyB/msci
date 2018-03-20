Fix rotation sense of orbit

geodesic_solver/orbit.py
@@ -87,7 +87,7 @@ class Orbit:
 
         v_orb = 2*np.pi*sma*sma/(np.linalg.norm(x_orb)*period) * \
                 np.array([-np.sin(ecc_anom),
-                          np.cos(ecc_anom)*np.sqrt(1-ecc*ecc), 0])
+                          -np.cos(ecc_anom)*np.sqrt(1-ecc*ecc), 0])
 
         x_bh = self.__bh.bh_from_obs(self.obs_from_orb(x_orb))
         v_bh = self.__bh.bh_from_obs(self.obs_from_orb(v_orb))

s2_precession.py
@@ -38,11 +38,47 @@ phi = np.linspace(0, 360, nphi)
 
 angle = np.zeros((ntheta, nphi))
 
+_ang = -1
+_theta = -1
+_phi = -1
 for i in range(ntheta):
     for j in range(nphi):
-        angle[i, j] = precession_angle(theta[i], phi[j])
+        ang = precession_angle(theta[i], phi[j])
+        angle[i, j] = ang
         if j == (nphi-1): print(i)
-        
+        if ang > _ang:
+            _ang = ang
+            _theta = theta[i]
+            _phi = phi[j]
+
+
+plt.close('all')
+
 cs = plt.contourf(phi, theta, angle,
                   50, cmap='viridis')
 plt.colorbar(cs, orientation='vertical')
+
+# plot spin for max precession with orbit
+bh = BlackHole(a=0.99, M=4.28e6, R_0=8.32, v_r=14.2,
+               spin_theta=_theta, spin_phi=_phi)
+
+zeta = np.linspace(0, bh.from_years(18), 500000)
+s2 = Orbit(bh=bh,
+           sma=0.1255,
+           ecc=0.8839,
+           incl=134.18,
+           long_asc=226.94,
+           arg_peri=65.51,
+           period=16.0,
+           zeta=zeta)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter([0],[0])
+ax.plot(s2.xyz[:, 0], s2.xyz[:, 1], zs=s2.xyz[:, 2])
+
+# spin direction
+_s = np.array([0,0,-10000])
+s = bh.obs_from_bh(_s)
+
+ax.plot([0,s[0]], [0,s[1]], zs=[0,s[2]])