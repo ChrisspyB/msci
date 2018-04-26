Fixed sign error in horiz_kerr_deflection. Did some figures stuff

horiz_kerr_deflection.py
@@ -28,7 +28,7 @@ for y in ys:
     rays.append(r)
 
     b = np.abs(y)
-    s = -np.sign(y)
+    s = np.sign(y)
     theory.append(4/b \
              + (15*np.pi/4 - 12*s*a) * 1/(b*b) \
              + (128/3 - 10*np.pi*s*a + 4*a*a) * 1/(b*b*b))

s2.py
@@ -10,8 +10,7 @@ PLOT = True
 bh = BlackHole(a=0.99, M=4.28e6, R_0=8.32, v_r=14.2, spin_theta=0, spin_phi=0)
 
 zeta = np.linspace(0, bh.from_years(18), 100000)
-#zeta = np.concatenate((np.linspace(0, bh.from_years(0.1), 2),
-#                       np.linspace(bh.from_years(16.3), bh.from_years(16.4), 10000)))
+#zeta = np.concatenate(([0], np.linspace(bh.from_years(8), bh.from_years(8.4), 100000)))
 
 s2 = Orbit(bh=bh,
            sma=0.1255,
@@ -24,7 +23,7 @@ s2 = Orbit(bh=bh,
 
 t = s2.orbit[:, 0]
 xyz = s2.xyz
-obs_t, deflec, fshift, dopp, grav = s2.earth_obs(8)
+obs_t, deflec, fshift, dopp, grav = s2.earth_obs(512)
 
 if PLOT:
     plt.close('all')
@@ -69,7 +68,8 @@ if PLOT:
     ax1.plot(bh.to_years(obs_t), bh.to_arcsec(deflec*1e6), color='b')
 
     ax2 = ax1.twinx()
-    ax2.set_ylabel("v_r (km/s)", color='r')
+    ax2.set_ylabel("Apparent v_r due to gravitational redshift (km/s)", color='r')
     ax2.tick_params(axis='y', labelcolor='r')
-    radial_vel = (1 - fshift*fshift)/(1 + fshift*fshift)
+    f = grav
+    radial_vel = (1 - f*f)/(1 + f*f)
     ax2.plot(bh.to_years(obs_t), bh.to_kms(radial_vel), color='r')

s2_precession.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from mpl_toolkits.mplot3d import Axes3D
+
 from geodesic_solver import BlackHole, Orbit
 
 def precession_angle(theta, phi):
@@ -36,6 +38,9 @@ nphi = ntheta
 theta = np.linspace(0, 180, ntheta)
 phi = np.linspace(0, 360, nphi)
 
+#theta = np.linspace(130, 140, ntheta)
+#phi = np.linspace(310, 320, nphi)
+
 angle = np.zeros((ntheta, nphi))
 
 _ang = -1
@@ -55,8 +60,10 @@ for i in range(ntheta):
 plt.close('all')
 
 cs = plt.contourf(phi, theta, angle,
-                  50, cmap='viridis')
+                  64, cmap='viridis')
 plt.colorbar(cs, orientation='vertical')
+plt.xlabel('ϕ / °')
+plt.ylabel('θ / °')
 
 # plot spin for max precession with orbit
 bh = BlackHole(a=0.99, M=4.28e6, R_0=8.32, v_r=14.2,